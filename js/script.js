2c0deb8bd954741f83337e721d2aab6861ef5216
CordobaFran <francocordobafs@gmail.com>
2022-06-06 19:56:15 -0300

    before pull

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ function inicio(){
         parrafo.innerHTML = `<h4 class="font-weight-bolder h2">${userFiltered.titular}</h4>
                             <h4>Cuenta N° ${userFiltered.numCuenta}</h4>
                             <h4>${currency(userFiltered.cantidad)}</h4>`;
-        divDatos.append(parrafo);     
+        divDatos.append(parrafo);
 }
 
 function cuenta(){
